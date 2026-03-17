abb0ab57b5e96cfed3ab60eca201264cfcd68bb2
Mariana D'Odorico <mariana8390@gmail.com>
2026-03-17 14:47:31 -0300

    Fix SMTP TLS

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def run():
     return jsonify({"status": "Búsqueda iniciada ✅"})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
